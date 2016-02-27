87e0c60391d0e603c7b2e3d48c5c6bdf8d3e44e0
lionswrath <caio.tonetti@gmail.com>
2016-02-26 22:57:39 -0300

    População inicializada

--- set_cover.cpp
+++ set_cover.cpp
@@ -4,6 +4,7 @@
 #include <vector>
 #include <cstring>
 #include <string>
+#include <random>
 
 typedef struct{
     std::vector<int> chromossome;
@@ -39,7 +40,7 @@ void insertIndex(set_index* indexes, set * new_set, int index) {
         indexes->operator[](i).push_back(index);
 }
 
-void readFile(std::string filename, data* datasets) {
+set_index readFile(std::string filename, data* datasets) {
     std::ifstream infile(filename);
     std::string line;
     char * cstr, * p;
@@ -60,7 +61,7 @@ void readFile(std::string filename, data* datasets) {
 
     std::getline(infile,line);
 
-    set_index indexes(N+1, std::vector<int>(N+1));
+    set_index indexes(N, std::vector<int>());
 
     while(std::getline(infile,line)) {
         set new_set;
@@ -73,26 +74,87 @@ void readFile(std::string filename, data* datasets) {
         p = std::strtok(NULL, " ");
         do {
             std::cout << p << " ";
-            new_set.setValues.push_back(atoi(p));
+            new_set.setValues.push_back(atoi(p) - 1);
             p = std::strtok(NULL, " ");  
         } while(p != NULL);
         std::cout << std::endl;
         datasets->push_back(new_set);
         insertIndex(&indexes, &new_set, datasets->size()-1);
     }
+
+    return indexes;
 }
 
-individual createIndividual() {
-    individual new_ind;
+void printDataset(data dataset) {
+    for (auto s : dataset) {
+        for (auto i : s.setValues)
+            std::cout << i << std::endl;
+        std::cout << s.weight << std::endl;
+    }
+}
 
+void printIndividual(individual ind) {
+    for (int i : ind.chromossome)
+        std::cout << i << " ";
+    std::cout << std::endl << ind.fitness << std::endl;  
+}
+
+void printIndexes(set_index indexes) {
+    for (auto vec : indexes) {
+        for (auto i : vec)
+            std::cout << i << " ";
+
+        std::cout << std::endl;
+    }
+    std::cout << "end" << std::endl;
+}
+
+void createIndividual(individual * new_ind, std::mt19937 * random_generator, set_index * indexes, 
+        data * datasets) {
+    double weight = 0;
+    double elements = 0;
+    
+    for (int i=0; i<N; i++) {
+        int set_value = indexes->operator[](i)[(*random_generator)() % indexes->operator[](i).size()];
+        new_ind->chromossome.push_back(set_value);
+        weight += datasets->at(i).weight;
+        elements += datasets->at(set_value).setValues.size();
+    }
+    new_ind->fitness = (elements - 50.0f)*weight;
+}
+
+void initializePopulation(int size, population_heap * population, std::mt19937 * random_generator, 
+        set_index * indexes, data * datasets) {
+    for (int i=0; i<size; i++) {
+        individual new_ind;
+        createIndividual(&new_ind, random_generator, indexes, datasets);
+        population->push(new_ind);
+    }
+}
+
+pair<individual,individual> chooseParents() {
 
 }
 
 int main() {
-    data datasets;
-    readFile("test_01.dat", &datasets);
 
+    std::mt19937 random_generator(125689566);
+
+    data datasets;
+    set_index indexes = readFile("test_01.dat", &datasets);
+
+    //printDataset(datasets);
+    printIndexes(indexes);
+
+    individual ind; 
+    createIndividual(&ind ,&random_generator, &indexes, &datasets);
+    
+    printIndividual(ind);  
+   
     population_heap population;
+    initializePopulation(500, &population, &random_generator, &indexes, &datasets);
+
+    std::cout << population.size() << std::endl;
 
     std::cout << datasets.size() << std::endl;
 }
